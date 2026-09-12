8bd870c77c7dae096f04396033e3a785dd4e44e3
zen811 <khatriharshit84@gmail.com>
2026-09-12 23:28:18 +0530

    text update debug 3

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -27,7 +27,7 @@ def draw_landmarks_group(frame, landmarks_list):
         for landmark in landmarks_list:
             x = int(landmark.x * frame.shape[1])            # Convert normalized relative coordinates to absolute pixel values
             y = int(landmark.y * frame.shape[0])
-            text=[landmark.x,landmark.y,landmark.z]
+            text=str(landmark.x)+","+str(landmark.y)+","+str(landmark.z) 
             cv.putText(frame,text,(x,y),cv.FONT_HERSHEY_SIMPLEX,1,(0, 255, 0),2)
             cv.circle(frame, (x, y), 3, (0, 255, 0), -1)            #Draw a tracking node dot
 
