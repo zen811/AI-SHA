19f9ffafa8b214cfcf34cde6d0d198168d14dd77
zen811 <khatriharshit84@gmail.com>
2026-09-13 10:25:19 +0530

    making of the model

---
+++ model_training/csv_maker.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+import mediapipe as mp
+import pandas as pd
+import cv2 as cv
+
+BaseOptions = mp.tasks.BaseOptions
+HolisticLandmarker = mp.tasks.vision.HolisticLandmarker
+HolisticLandmarkerOptions = mp.tasks.vision.HolisticLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+model_path = './AI-SHA/holistic_landmarker.task'
+folder_path = Path("./AI-SHA/model_training/gestures/hello")
+file_list = [file for file in folder_path.iterdir() if file.is_file()]
+
+
+options = HolisticLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.IMAGE,
+    min_face_detection_confidence=0.5,
+    min_pose_detection_confidence=0.5,
+    min_hand_landmarks_confidence=0.5)
+
+
+
+
+landmark_hello=[]
+with HolisticLandmarker.create_from_options(options) as landmarker:
+    for file in file_list:
+        mp_image = mp.Image.create_from_file(str(file))
+        result = landmarker.detect(mp_image)
+        landmarkdata=[]
+        for idx, landmark in enumerate(result.pose_landmarks):
+            print(f"For Poses\nIndex {idx:02d} -> X: {landmark.x:.4f}, Y: {landmark.y:.4f}, Z: {landmark.z:.4f}")
+            landmarkdata.append(idx)
+            landmarkdata.append(landmark.x)
+            landmarkdata.append(landmark.y)
+            landmarkdata.append(landmark.z)
+        for idx,landmark in enumerate(result.left_hand_landmarks):
+            print(f"For Poses\nIndex {idx:02d} -> X: {landmark.x:.4f}, Y: {landmark.y:.4f}, Z: {landmark.z:.4f}")
+            landmarkdata.append(idx)
+            landmarkdata.append(landmark.x)
+            landmarkdata.append(landmark.y)
+            landmarkdata.append(landmark.z)
+        for idx,landmark in enumerate(result.right_hand_landmarks):
+            print(f"For Poses\nIndex {idx:02d} -> X: {landmark.x:.4f}, Y: {landmark.y:.4f}, Z: {landmark.z:.4f}")
+            landmarkdata.append(idx)
+            landmarkdata.append(landmark.x)
+            landmarkdata.append(landmark.y)
+            landmarkdata.append(landmark.z)
+        landmarkdata.append(0)
+        landmark_hello.append(landmarkdata)
+
+df = pd.DataFrame(landmark_hello)
+df.to_csv("Hello.csv", index=False)
+
+
+
+
+
