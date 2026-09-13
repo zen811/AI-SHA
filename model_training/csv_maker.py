5ee9714268d628a90d12c5ff8386a688641f8560
zen811 <khatriharshit84@gmail.com>
2026-09-13 14:07:08 +0530

    test new

--- model_training/csv_maker.py
+++ model_training/csv_maker.py
@@ -9,8 +9,8 @@ HolisticLandmarkerOptions = mp.tasks.vision.HolisticLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-model_path = './AI-SHA/model_training/holistic_landmarker.task'
-folder_path = Path("./AI-SHA/model_training/gestures/hello")
+model_path = './model_training/holistic_landmarker.task'
+folder_path = Path("./model_training/gestures/hello")
 file_list=[]
 for file in folder_path.iterdir():
     if file.is_file():
