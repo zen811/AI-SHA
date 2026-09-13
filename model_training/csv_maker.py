7c3cf8bf75d2f1552486c0e1525d40e9db16c49b
zen811 <khatriharshit84@gmail.com>
2026-09-13 14:48:25 +0530

    test commit

--- model_training/csv_maker.py
+++ model_training/csv_maker.py
@@ -40,12 +40,6 @@ with HolisticLandmarker.create_from_options(options) as landmarker:
             landmarkdata.append(landmark.x)
             landmarkdata.append(landmark.y)
             landmarkdata.append(landmark.z)
-        for idx,landmark in enumerate(result.left_hand_landmarks):
-            print(f"image{id:02d} left hand \nIndex {idx:02d} -> X: {landmark.x:.4f}, Y: {landmark.y:.4f}, Z: {landmark.z:.4f}")
-            landmarkdata.append(idx)
-            landmarkdata.append(landmark.x)
-            landmarkdata.append(landmark.y)
-            landmarkdata.append(landmark.z)
         for idx,landmark in enumerate(result.right_hand_landmarks):
             print(f"image{id:02d} right hand\nIndex {idx:02d} -> X: {landmark.x:.4f}, Y: {landmark.y:.4f}, Z: {landmark.z:.4f}")
             landmarkdata.append(idx)
