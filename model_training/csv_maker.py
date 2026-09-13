4fa065d74a37227f8889daaac7d2c447a6ce3417
Mithlesh Kumar Gupta <mithlesh735222@gmail.com>
2026-09-14 00:30:08 +0530

    Sorry Bye

--- model_training/csv_maker.py
+++ model_training/csv_maker.py
@@ -9,7 +9,7 @@ HolisticLandmarkerOptions = mp.tasks.vision.HolisticLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 model_path = str(Path("./model_training/holistic_landmarker.task").resolve())
-folder_path = Path("./model_training/gestures/Thank_u").resolve()
+folder_path = Path("./model_training/gestures/Sorry").resolve()
 
 file_list = [
     str(file.resolve())
@@ -71,8 +71,8 @@ with HolisticLandmarker.create_from_options(options) as landmarker:
     else:
       landmarkdata.extend([0.0] * (21 * 3))
 
-    landmarkdata.append(1)
+    landmarkdata.append(2)
     landmark_hello.append(landmarkdata)
 
 df = pd.DataFrame(landmark_hello)
-df.to_csv("./model_training/Thank_u.csv", index=False)
+df.to_csv("./model_training/Sorry.csv", index=False)
